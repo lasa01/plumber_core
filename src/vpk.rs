d7ffedf83cb33c70e8990a87284f49b7da1428ff
Lassi Säike <lassisaike11@gmail.com>
2021-05-01 19:48:46 +0300

    vpk: refactor, add tests, fix string parsing

--- src/vpk.rs
+++ src/vpk.rs
@@ -8,7 +8,7 @@ use zerocopy::{
 };
 use crc::crc32;
 
-#[derive(FromBytes, Unaligned)]
+#[derive(Debug, PartialEq, FromBytes, Unaligned)]
 #[repr(C)]
 struct HeaderV1 {
     signature: U32<LE>,
@@ -16,7 +16,7 @@ struct HeaderV1 {
     tree_size: U32<LE>,
 }
 
-#[derive(FromBytes, Unaligned)]
+#[derive(Debug, PartialEq, FromBytes, Unaligned)]
 #[repr(C)]
 struct HeaderV2Ext {
     file_data_section_size: U32<LE>,
@@ -25,7 +25,7 @@ struct HeaderV2Ext {
     signature_section_size: U32<LE>,
 }
 
-#[derive(FromBytes, Unaligned)]
+#[derive(Debug, PartialEq, FromBytes, Unaligned)]
 #[repr(C)]
 struct DirectoryEntry {
     crc: U32<LE>,
@@ -36,7 +36,7 @@ struct DirectoryEntry {
     terminator: U16<LE>,
 }
 
-#[derive(FromBytes, Unaligned)]
+#[derive(Debug, PartialEq, FromBytes, Unaligned)]
 #[repr(C)]
 struct ArchiveMd5SectionEntry {
     archive_index: U32<LE>,
@@ -45,7 +45,7 @@ struct ArchiveMd5SectionEntry {
     md5_checksum: [u8; 16],
 }
 
-#[derive(FromBytes, Unaligned)]
+#[derive(Debug, PartialEq, FromBytes, Unaligned)]
 #[repr(C)]
 struct OtherMd5Section {
     tree_checksum: [u8; 16],
@@ -53,11 +53,42 @@ struct OtherMd5Section {
     unknown: [u8; 16],
 }
 
+struct TreeEntry {
+    crc: u32,
+    archive_index: u16,
+    entry_offset: u32,
+    entry_length: u32,
+    preload_bytes: Vec<u8>,
+}
+
+impl TreeEntry {
+    fn parse(bytes: &mut &[u8]) -> Result<Self, ParseError> {
+        let entry: LayoutVerified<_, DirectoryEntry> = parse(bytes).ok_or(ParseError::EntryEof)?;
+        let preload = entry.preload_bytes.get().into();
+        if preload > bytes.len() {
+            return Err(ParseError::PreloadEof);
+        }
+        let preload_data = {
+            let (data, remaining) = bytes.split_at(preload);
+            *bytes = remaining;
+            data
+        };
+
+        Ok(Self {
+            crc: entry.crc.get(),
+            archive_index: entry.archive_index.get(),
+            entry_offset: entry.entry_offset.get(),
+            entry_length: entry.entry_length.get(),
+            preload_bytes: preload_data.to_vec(),
+        })
+    }
+}
+
 #[derive(Debug, Error)]
 pub enum ParseError {
     #[error("encountered eof reading header")]
     HeaderEof,
-    #[error("invalid signature")]
+    #[error("not a vpk (invalid signature)")]
     InvalidSignature,
     #[error("unsupported version {0}")]
     UnsupportedVersion(u32),
@@ -75,13 +106,8 @@ pub enum ParseError {
     ChecksumMismatch,
 }
 
-pub struct Directory {
-    tree: HashMap<String, TreeEntry>,
-    tree_size: usize,
-}
-
 fn parse_nul_str<'a>(bytes: &mut &'a [u8]) -> Option<&'a str> {
-    let mut split = bytes.splitn(1, |&b| b == 0);
+    let mut split = bytes.splitn(2, |&b| b == 0);
     let str_bytes = split.next()?;
     if str_bytes == b"\0" {
         return None;
@@ -96,6 +122,11 @@ fn parse<'a, T: Unaligned>(bytes: &mut &'a [u8]) -> Option<LayoutVerified<&'a [u
     let (verified, remaining) = LayoutVerified::new_unaligned_from_prefix(*bytes)?;
     *bytes = remaining;
     Some(verified)
+}
+
+pub struct Directory {
+    tree: HashMap<String, TreeEntry>,
+    tree_size: usize,
 }
 
 impl Directory {
@@ -180,37 +211,88 @@ impl Directory {
             }
         }
 
-        Ok(Self { tree, tree_size: header.tree_size.get() as usize })
+        Ok(Self { tree, tree_size: tree_len })
     }
 }
 
-struct TreeEntry {
-    crc: u32,
-    archive_index: u16,
-    entry_offset: u32,
-    entry_length: u32,
-    preload_bytes: Vec<u8>,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-impl TreeEntry {
-    fn parse(bytes: &mut &[u8]) -> Result<Self, ParseError> {
-        let entry: LayoutVerified<_, DirectoryEntry> = parse(bytes).ok_or(ParseError::EntryEof)?;
-        let preload = entry.preload_bytes.get().into();
-        if preload > bytes.len() {
-            return Err(ParseError::PreloadEof);
-        }
-        let preload_data = {
-            let (data, remaining) = bytes.split_at(preload);
-            *bytes = remaining;
-            data
+    #[test]
+    fn test_vpk_parse_header() {
+        let mut header = &[
+            0x34u8, 0x12, 0xaa, 0x55,
+            0x02, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x30, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+        ][..];
+        let parsed = HeaderV1 {
+            signature: 0x55aa_1234.into(),
+            version: 2.into(),
+            tree_size: 0.into(),
         };
+        let parsed_ext = HeaderV2Ext {
+            file_data_section_size: 0.into(),
+            archive_md5_section_size: 0.into(),
+            other_md5_section_size: 48.into(),
+            signature_section_size: 0.into(),
+        };
+        assert_eq!(
+            parse::<HeaderV1>(&mut header).unwrap().into_ref(),
+            &parsed,
+        );
+        assert_eq!(
+            parse::<HeaderV2Ext>(&mut header).unwrap().into_ref(),
+            &parsed_ext,
+        );
+    }
 
-        Ok(Self {
-            crc: entry.crc.get(),
-            archive_index: entry.archive_index.get(),
-            entry_offset: entry.entry_offset.get(),
-            entry_length: entry.entry_length.get(),
-            preload_bytes: preload_data.to_vec(),
-        })
+    #[test]
+    fn test_parse_tree() {
+        let mut tree = &[
+            0x76u8, 0x6d, 0x74, 0x00, // extension
+            0x6d, 0x61, 0x74, 0x65, // filepath
+            0x72, 0x69, 0x61, 0x6c,
+            0x73, 0x2f, 0x6d, 0x6f,
+            0x64, 0x65, 0x6c, 0x73,
+            0x2f, 0x70, 0x72, 0x6f,
+            0x70, 0x73, 0x5f, 0x76,
+            0x65, 0x68, 0x69, 0x63,
+            0x6c, 0x65, 0x73, 0x00,
+            0x62, 0x31, 0x37, 0x00, // filename
+            0xE5, 0x3e, 0x0e, 0x95, // crc
+            0x00, 0x00, 0xFF, 0x7F, // preload bytes, archive index
+            0x54, 0x3D, 0x3A, 0x02, // entry offset
+            0x47, 0x00, 0x00, 0x00, // entry length
+            0xFF, 0xFF,             // terminator
+            0x00, 0x00,  0x00,      // tree end
+        ][..];
+        assert_eq!(
+            parse_nul_str(&mut tree),
+            Some("vmt"),
+        );
+        assert_eq!(
+            parse_nul_str(&mut tree),
+            Some("materials/models/props_vehicles"),
+        );
+        assert_eq!(
+            parse_nul_str(&mut tree),
+            Some("b17"),
+        );
+        assert_eq!(
+            parse::<DirectoryEntry>(&mut tree).unwrap().into_ref(),
+            &DirectoryEntry {
+                crc: 2500738789.into(),
+                preload_bytes: 0.into(),
+                archive_index: 0x7fff.into(),
+                entry_offset: 37371220.into(),
+                entry_length: 71.into(),
+                terminator: 0xffff.into(),
+            },
+        );
     }
 }
